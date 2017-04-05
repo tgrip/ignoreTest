94c3a258310a7a79834989eceec798bc18bfd838
Theodor Grip <theodor.grip@1und1.de>
2017-04-05 10:56:11 +0200

    Initial commit
    Simple ignored test

---
+++ src/test/java/com/oneandone/ExampleTest.java
@@ -0,0 +1,25 @@
+/*
+ * ExampleTest.java 05.04.17
+ *
+ * Copyright (c) 2017 1&1 Internet AG, Karlsruhe. All rights reserved.
+ *
+ * $Id$
+ */
+package com.oneandone;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author tgrip
+ */
+@Ignore
+public class ExampleTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(7, 3 + 4);
+    }
+}
